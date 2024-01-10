792678d42958a8e587b90d11970bfcaa50845d26
PLTNV07 <147551901+PLTNV07@users.noreply.github.com>
2024-01-10 19:23:06 +0300

    Update dict_bot.py

--- dict_bot.py
+++ dict_bot.py
@@ -5,7 +5,7 @@
 
 from telebot import TeleBot, types
 
-bot = TeleBot(token='6832909531:AAEnEG_19pREbU-Xm4u9T-VaPV-MqBcJw5o', parse_mode='html') # создание бота
+bot = TeleBot(token='', parse_mode='html') # создание бота
 
 # словарь с определениями и аббревиатурами, которые знает бот
 # в формате:
